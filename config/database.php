1a5e4cfe2db387434aa0dfe8821b1691358f3444
Yudhistira <yudhistira30122008@gmail.com>
2026-05-27 18:49:51 +0700

    feat: add database configuration file for MySQL, PostgreSQL, and Redis support

--- config/database.php
+++ config/database.php
@@ -21,6 +21,19 @@ return [
             'engine' => null,
         ],
 
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'postgres'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
     ],
 
     'migrations' => 'migrations',
